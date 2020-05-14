423fa1158ecca6fbb16a6b9760a130e0b0ecf7bb
bitcyber <rakysh.inc@gmail.com>
2020-05-14 23:42:13 +0530

    Leetcode #13

---
+++ 13.RomanToInteger.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int result = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (i < s.size() - 1 && romanCharToInt(s[i]) < romanCharToInt(s[i + 1]) )
+                result -= romanCharToInt(s[i]);
+            else 
+                result += romanCharToInt(s[i]);
+        }
+        return result;
+    }
+        int romanCharToInt(char c) {
+            switch(c) {
+                    case 'I': return 1;
+                    case 'V':	return 5;
+                    case 'X':	return 10;
+                    case 'L':	return 50;
+                    case 'C':	return 100;
+                    case 'D':	return 500;
+                    case 'M':	return 1000;
+                    default:	return 0;
+            }
+        } 
+};
