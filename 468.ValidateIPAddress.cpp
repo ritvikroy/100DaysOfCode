2b70c68e794d1d01fb129e60e5e27f3df3b96d32
bitcyber <rakysh.inc@gmail.com>
2020-06-17 10:58:57 +0530

    Leetcode #468

---
+++ 468.ValidateIPAddress.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string validIPAddress(string IP) {
+        regex ipv4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])");
+        
+        regex ipv6("((([0-9a-fA-F]){1,4})\\:){7}([0-9a-fA-F]){1,4}");   
+        
+        if(regex_match(IP, ipv4)) 
+            return "IPv4";
+        else if(regex_match(IP, ipv6)) 
+            return "IPv6";
+        return "Neither"; 
+    }
+};
