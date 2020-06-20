50bf165d81704d439c3b4314c7cfbe7cd5671432
bitcyber <rakysh.inc@gmail.com>
2020-06-20 22:33:39 +0530

    Leetcode #268

---
+++ 268.MissingNumber.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {     
+        int n = nums.size();
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+              sum += nums[i];
+        }  
+        return (n*(n + 1))/2 - sum;
+    }
+};
