e06d065fff023b18113b11fbaafffb31dd25340f
bitcyber <rakysh.inc@gmail.com>
2020-07-05 22:05:33 +0530

    Leetcode #461

---
+++ 463.HammingDistance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int result = 0;
+       while(x > 0 || y > 0) {
+           result += (x%2) ^ (y%2);
+           x >>= 1;
+           y >>= 1;
+       }
+        return result;
+    }
+};
