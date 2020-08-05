24e5a9bde8d93a2cb5d740a8371c1165a33adcb3
bitcyber <rakysh.inc@gmail.com>
2020-08-05 21:45:26 +0530

    Leetcode #1534

---
+++ 1534.CountGoodTriplets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int result {};
+        for (int i = 0; i < arr.size(); i++){
+            for(int j = i+1;j < arr.size();j++){
+                if(abs(arr[i]-arr[j]) > a){
+                    continue;
+                }
+                for(int k = j+1;k < arr.size();k++){
+                    if(abs(arr[i]-arr[j]) <= a && abs(arr[j]-arr[k]) <= b &&    abs(arr[k]-arr[i]) <= c){
+                        result += 1;
+                    }
+                }
+            }
+        }
+        return result;   
+    }
+};
